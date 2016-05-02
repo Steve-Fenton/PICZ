f8776c9d77ad42936b01385b4292dba3cce56301
Steve Fenton <github@stevefenton.co.uk>
2016-05-02 15:51:27 +0100

    Remove disk check from example to avoid confusion.

--- Picz/Controllers/PiczController.cs
+++ Picz/Controllers/PiczController.cs
@@ -11,10 +11,6 @@ namespace Picz.Controllers
         [Route("Picz")]
         public ActionResult Picz(int s, string p)
         {
-            // Optional - disk check is available here... you can use this to write to 
-            // your preferred logging mechanism to get early warnings about disk space.
-            var freeSpace = DiskInformation.GetDiskSpaceInSIMegabytes("C:\\");
-
             var originalUrl = new Uri(Request.Url, p).AbsoluteUri;
             var replacement = _imageResizer.GetReplacementImage(s, originalUrl);
             return File(replacement.Path, replacement.MimeType);
